3557339e438441ec4715fc123755fb26ea718890
vishalcreatiosoft <vishal.srivastava@creatiosoft.com>
2021-10-28 16:25:39 +0530

    final Commit

--- news/public/js/validation.js
+++ news/public/js/validation.js
@@ -29,20 +29,6 @@ for(let i=0;i<newsInfo.length;i++)
     
 }
 
-// const source = document.createElement('h2')
-// source.textContent = newsInfo[0].source
-// document.querySelector('#row-home').appendChild(source)
 
-// const title = document.createElement('h3')
-// title.textContent = 'Title :' + newsInfo[0].title
-// document.querySelector('#row-home').appendChild(title)
-
-// const content = document.createElement('p')
-// content.textContent = 'Article :' + newsInfo[0].content
-// document.querySelector('#row-home').appendChild(content)
-
-// const url = document.createElement('a')
-// url.textContent = newsInfo[0].url
-// document.querySelector('#row-home').appendChild(url).setAttribute('href',url.textContent)
 
 
