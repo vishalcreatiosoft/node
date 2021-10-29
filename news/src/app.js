fae85ab4f71c9206479b1bbc72731af237b21b47
vishalcreatiosoft <vishal.srivastava@creatiosoft.com>
2021-10-29 11:49:28 +0530

    final commit

--- news/src/app.js
+++ news/src/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const request = require('request')
 const path = require('path')
+const { response } = require('express')
+const fetch = require('cross-fetch')
+const { resolve, parse } = require('path')
+const { rejects } = require('assert')
 
-
+//how to set the port
 const port = process.env.PORT || 3000
 const app = express()
 
@@ -16,41 +20,48 @@ app.set('views',viewsDirectoryPath)
 app.set('view engine','hbs')
 app.use(express.static(publicDirectoryPath))
 
+//code below is to set url result to show
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 
-
+//simple route
 app.get('',(req,res)=>{
     res.render('index')
 })
 
-app.post('/home',(req, res)=>{
-    let dataArray = []
+
+//async function to fetch the data and return to post route
+const fetchData = async(country, category)=>{
+    let dataArray =[]
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&sortBy=popularity&apiKey=65d1657a27174dbabb6ad8d40bd8fe3f&limit=1`  
+
+    const data = await fetch(url).then((response)=> response.json())
+    
+    for(let i=0;i<20;i++){
+        dataArray.push({
+            source : data.articles[i].source.name,
+            title : data.articles[i].title,
+            url : data.articles[i].url,
+            content : data.articles[i].description
+        })
+    }    
+    //console.log(dataArray)
+    return dataArray
+    
+}
+
+app.post('/home',async(req, res)=>{
+    
     const country = req.body.countryName
     const category = req.body.newsCategory
+    const getData = await fetchData(country, category)
+    //console.log(result)
 
-    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&sortBy=popularity&apiKey=65d1657a27174dbabb6ad8d40bd8fe3f&limit=1`
+    resultData = JSON.stringify(getData) 
+    res.render('home',{newsInfo : resultData })
 
-    request({url: url},(error,response)=>{
-        const data = JSON.parse(response.body)
-
-        for(let i=0;i<20;i++){
-            dataArray.push({
-                source : data.articles[i].source.name,
-                title : data.articles[i].title,
-                url : data.articles[i].url,
-                content : data.articles[i].description
-            })
-        }  
-        
-        resultData = JSON.stringify(dataArray)
-        
-        res.render('home',{newsInfo : resultData })
-    })
     
-
 })
-
 
 
 
@@ -58,3 +69,4 @@ app.post('/home',(req, res)=>{
 app.listen(port,()=>{
     console.log(`server is running on ${port} `)
 })
+
