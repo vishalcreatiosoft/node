aae17073bae193f632496f8dc3f5f323900ff799
vishalcreatiosoft <vishal.srivastava@creatiosoft.com>
2021-11-09 12:54:39 +0530

    first commit

---
+++ my-shop/src/app.js
@@ -0,0 +1,29 @@
+const e = require('express')
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const routes = require('../routes/route')
+
+const app = express()
+const port = process.env.PORT || 3000
+const views_path = path.join(__dirname, '../views')
+const public_path = path.join(__dirname, '../public')
+
+app.set('views', views_path)
+app.set('view engine', 'hbs')
+app.use(express.static(public_path))
+
+app.use(routes)
+
+mongoose.connect('mongodb://localhost:27017/myshop')
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port no ${port}`)
+})
